d7d60908b2bb58bd273ee03c8cbdf116e4cfa8c3
behzadsoleimani <behzadsoleimani97@gmail.com>
2022-08-20 16:06:49 +0430

    improve types and create list component

---
+++ src/components/List/index.tsx
@@ -0,0 +1,280 @@
+import React, { useState } from 'react';
+import { Droppable, Draggable } from 'react-beautiful-dnd';
+import styled from 'styled-components';
+import Button from '../Button';
+import ListCard from '../ListCard';
+import ListTitleButton from '../ListTitleButton';
+import DeleteListButton from '../DeleteListButton';
+import DeleteCardButton from '../DeleteCardButton';
+import EditCardButton from '../EditCardButton';
+import CardTextarea from '../CardTextarea';
+import ListTitleTextarea from '../ListTitleTextarea';
+import { useAppDispatch, useAppSelector } from '../../redux/hook';
+import { addCard, editListTitle } from '../../redux/board';
+
+// import {addCard, editCardTitle, deleteCard, editListTitle, deleteList} from '../../actions/actionCreators';
+
+const TextareaWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-end;
+  margin: 0 10px;
+`;
+
+const ListTitleTextareaWrapper = styled.div`
+  height: 48px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 0 10px;
+`;
+
+const CardTextareaWrapper = styled(TextareaWrapper)`
+  margin: 0 10px 10px 10px;
+`;
+
+const ComposerWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  background: #f8f8f8;
+  padding: 0 0 10px 0;
+  border: none;
+  border-bottom-left-radius: 4px;
+  border-bottom-right-radius: 4px;
+`;
+
+const ListTitle = styled.div`
+  display: flex;
+  flex-shrink: 0;
+  height: 48px;
+  align-items: center;
+  color: rgb(46, 68, 78);
+`;
+
+const CardTitle = styled.div`
+  background: white;
+  box-shadow: inset 0 0 0 2px rgba(0, 0, 0, 0.1);
+  margin: 0 10px 10px 10px;
+  padding: 8px;
+  border-radius: 5px;
+  position: relative;
+  overflow-wrap: break-word;
+  overflow: visible;
+  word-wrap: break-word;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  &:hover,
+  &:active,
+  &:focus {
+    box-shadow: inset 0 0 0 2px rgba(0, 0, 0, 0.3);
+  }
+`;
+
+const ButtonWrapper = styled.div`
+  height: 16px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const List = ({ boardId, list }: any) => {
+  const [newCardFormIsOpen, setNewCardFormIsOpen] = useState(false);
+  const [isListTitleInEdit, setIsListTitleInEdit] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [cardInEdit, setCardInEdit] = useState(null);
+  const [newCardTitle, setNewCardTitle] = useState('');
+  const [newListTitle, setNewListTitle] = useState('');
+  const [tempCardTitle, setTempCardTitle] = useState('');
+  const dispatch = useAppDispatch();
+  const boards = useAppSelector(state => state.boards);
+  const currentBoard = boards.filter(board => board.id === boardId);
+  const currentList = currentBoard[0]!.list!.filter(l => l.id === list.id);
+  const cards = currentList![0]!.cards;
+  const toggleCardComposer = () => setNewCardFormIsOpen(!newCardFormIsOpen);
+
+  const handleCardComposerChange = (event: any) => {
+    setNewCardTitle(event.target.value);
+  };
+
+  const handleKeyDown = (event: any, callback: any) => {
+    if (event.keyCode === 13) {
+      callback(event);
+    }
+  };
+
+  const handleSubmitCard = (event: any) => {
+    event.preventDefault();
+    setNewCardFormIsOpen(false);
+    if (newCardTitle.length < 1) return;
+    onSubmitCard();
+  };
+
+  const openCardEditor = (event: any, card: any) => {
+    event.preventDefault();
+    setCardInEdit(card.id);
+    setTempCardTitle(card.title);
+  };
+
+  const handleCardEditorChange = (event: any) => {
+    setTempCardTitle(event.target.value);
+  };
+
+  const handleListTitleEditorChange = (event: any) => {
+    setNewListTitle(event.target.value);
+  };
+
+  const handleCardEdit = async (e: any) => {
+    e.preventDefault();
+    if (tempCardTitle.length < 1) {
+      onDeleteCard(cardInEdit);
+    } else {
+      onEditCard();
+    }
+  };
+
+  const handleDeleteCard = (event: any, cardId: any) => {
+    event.preventDefault();
+    onDeleteCard(cardId);
+  };
+
+  const openTitleEditor = () => {
+    setIsListTitleInEdit(true);
+    setNewListTitle(list.title);
+  };
+
+  const handleSubmitListTitle = () => {
+    if (newListTitle.length < 1) {
+      setIsListTitleInEdit(false);
+      return;
+    }
+    onEditListTitle(newListTitle.trim(), list.id, boardId);
+  };
+
+  const handleDeleteListButtonClick = (event: any) => {
+    event.preventDefault();
+    onDeleteList(list.cards, list.id, boardId);
+  };
+
+  const onSubmitCard = async () => {
+    setIsLoading(true);
+    await dispatch(addCard({ cardTitle: newCardTitle, listId: list.id, boardId }));
+    setIsLoading(false);
+    setNewCardTitle('');
+  };
+
+  const onEditCard = async () => {
+    // await dispatch(
+    //   editCardTitle({
+    //     cardTitle: tempCardTitle.trim(),
+    //     cardId: cardInEdit,
+    //     cardIndex: list.cards.indexOf(cardInEdit),
+    //     listId: list.id,
+    //     boardId
+    //   })
+    // ).then(() => {
+    //   setTempCardTitle('');
+    //   setCardInEdit(null);
+    // });
+  };
+
+  const onDeleteCard = (cardId: any) => {
+    // dispatch(deleteCard({cardId, listId: list.id, boardId}));
+  };
+
+  const onEditListTitle = async (listTitle: any, listId: any, boardId: any) => {
+    setIsListTitleInEdit(true);
+    await dispatch(editListTitle({listTitle, listId, boardId}))
+      setNewListTitle('');
+      setIsListTitleInEdit(false);
+  };
+
+  const onDeleteList = (cards: any, listId: any, boardId: any) => {
+    // dispatch(deleteList({cards, listId, boardId}));
+  };
+
+
+  return (
+    <ListCard>
+      {isListTitleInEdit ? (
+        <ListTitleTextareaWrapper>
+          <ListTitleTextarea
+            value={newListTitle}
+            onChange={handleListTitleEditorChange}
+            onKeyDown={(e: any) => handleKeyDown(e, handleSubmitListTitle)}
+            onBlur={handleSubmitListTitle}
+          />
+        </ListTitleTextareaWrapper>
+      ) : (
+        <ListTitle>
+          <ListTitleButton onFocus={openTitleEditor} onClick={openTitleEditor} text={list.title} />
+          <DeleteListButton onClick={(e: any) => handleDeleteListButtonClick(e)} />
+        </ListTitle>
+      )}
+      <Droppable droppableId={list.id}>
+        {(provided) => (
+          <div ref={provided.innerRef}>
+            {(cards || []).map((card: any, index: any) => (
+              <Draggable key={card.id} draggableId={card.id} index={index}>
+                {({ innerRef, draggableProps, dragHandleProps, placeholder }: any) => (
+                  <div>
+                    {cardInEdit !== card.id ? (
+                      <CardTitle
+                        ref={innerRef}
+                        {...draggableProps}
+                        {...dragHandleProps}
+                        data-react-beautiful-dnd-draggable="0"
+                        data-react-beautiful-dnd-drag-handle="0">
+                        {card.title}
+                        <ButtonWrapper>
+                          <DeleteCardButton onClick={(e: any) => handleDeleteCard(e, card.id)} />
+                          <EditCardButton onClick={(e: any) => openCardEditor(e, card)} />
+                        </ButtonWrapper>
+                      </CardTitle>
+                    ) : (
+                      <TextareaWrapper ref={innerRef} {...draggableProps} {...dragHandleProps}>
+                        <CardTextarea
+                          value={tempCardTitle}
+                          onChange={handleCardEditorChange}
+                          onKeyDown={(e: any) => handleKeyDown(e, handleCardEdit)}
+                          onBlur={handleCardEdit}
+                        />
+                      </TextareaWrapper>
+                    )}
+                    {placeholder}
+                  </div>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+            {(newCardFormIsOpen || isLoading) && (
+              <CardTextareaWrapper>
+                <CardTextarea
+                  value={newCardTitle}
+                  onChange={handleCardComposerChange}
+                  onKeyDown={(e: any) => handleKeyDown(e, handleSubmitCard)}
+                  onBlur={handleSubmitCard}
+                />
+                <Button variant="add" onClick={handleSubmitCard} text="Add" disabled={newCardTitle === ''} />
+              </CardTextareaWrapper>
+            )}
+            {!newCardFormIsOpen &&
+              !isLoading && (
+                <ComposerWrapper>
+                  <Button variant="card" text="Add new card" onClick={toggleCardComposer}>
+                    Add new card
+                  </Button>
+                </ComposerWrapper>
+              )}
+          </div>
+        )}
+      </Droppable>
+    </ListCard>
+  );
+};
+
+
+export default List;
--- src/types/global-types.ts
+++ src/types/global-types.ts
@@ -1,4 +1,18 @@
 export interface IBoard {
     title: string;
-    id: number
+    id: string,
+    list?: IList[];
+    cards?: ICard[];
+}
+
+
+export interface IList {
+    title: string;
+    id: string,
+    cards?: ICard[];
+}
+
+export interface ICard {
+    title: string;
+    id: string
 }
